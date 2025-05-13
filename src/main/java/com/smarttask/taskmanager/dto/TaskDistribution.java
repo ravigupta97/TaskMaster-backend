11c2e5d9d85b4660d5a98cd335bef39f1052158c
ravigupta.97 <rgupta1097@gmail.com>
2025-05-13 23:59:22 +0530

    Updated files

--- src/main/java/com/smarttask/taskmanager/dto/TaskDistribution.java
+++ src/main/java/com/smarttask/taskmanager/dto/TaskDistribution.java
@@ -1,11 +1,10 @@
 package com.smarttask.taskmanager.dto;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class TaskDistribution {
 
